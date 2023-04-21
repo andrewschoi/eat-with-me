c0e115248400aa4c1db2c15a94592bccbbfb33b0
andrew <asc269@cornell.edu>
2023-04-21 12:33:14 -0400

    Add env variable to firebase/config.js

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import Constants from "expo-constants"
+import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} from "@env"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: Constants.manifest?.extra?.API_KEY,
-  authDomain: Constants.manifest?.extra?.AUTH_DOMAIN,
-  projectId: Constants.manifest?.extra?.PROJECT_ID,
-  storageBucket: Constants.manifest?.extra?.STORAGE_BUCKET,
-  messagingSenderId: Constants.manifest?.extra?.MESSENGER_SENDER_ID,
-  appId: Constants.manifest?.extra?.APP_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 // Initialize Firebase
