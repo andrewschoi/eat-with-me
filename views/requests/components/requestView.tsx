d0dcc11c8c79929ee218f9f4037e524a4dbdd220
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-24 13:31:10 -0400

    [fe] Styles for add request button

--- views/requests/components/requestView.tsx
+++ views/requests/components/requestView.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, ScrollView, Button, Text, Pressable } from "react-native";
+import { View, ScrollView, Button, Text, Pressable, StyleSheet } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 import RequestRow from "./requestRow";
 import { StackScreenProps } from "@react-navigation/stack";
 import { EatRequest } from "../../../firebase/types";
@@ -23,11 +24,25 @@ const RequestView = ({ navigation, openRequests }: RequestViewProps) => {
           />
         ))}
       </ScrollView>
-      <Pressable onPress={handleNavigateToForm}>
-        <Text>Create request</Text>
-      </Pressable>
+
+      <View style={styles.addButton}>
+        <AntDesign onPress={handleNavigateToForm} name="pluscircle" size={32} color="#007AFF" />
+      </View>
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  addButton: {
+    backgroundColor: "black",
+    padding: 10,
+    width: 55,
+    overflow: "hidden",
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    marginTop: 10,
+  }
+});
 
 export default RequestView;
