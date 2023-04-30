d2eb945096bbdf65c17499eeededf39801bf8b0b
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-04-29 23:28:45 -0400

    Update message formatting

--- views/requests/components/matchView.tsx
+++ views/requests/components/matchView.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
-import { View, SafeAreaView, Text, TextInput, Pressable, StyleSheet, ScrollView } from "react-native";
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { View, SafeAreaView, Text, TextInput, Pressable, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { PendingMatch, Message } from "../../../firebase/types";
 import userContext from "../../../contexts/userContext";
 import * as BE from "../../../firebase/common";
@@ -38,13 +38,33 @@ const MatchView = () => {
     return () => unsubscribe.forEach((unsub) => unsub());
   }, []);
 
+  const scrollViewRef = useRef<ScrollView | null>(null);
+
+  const scrollToBottom = () => {
+    scrollViewRef.current?.scrollToEnd({ animated: true });
+  };
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-      <MessageView messages={messages} />
-      <ClearPendingMatch pendingMatch={pendingMatch} />
-      <TextField pendingMatch={pendingMatch} />
-      </ScrollView>
+    <SafeAreaView style={{flex: 1}}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingView}
+      >
+        <View style={{flex: 1}}>
+          <ScrollView
+            style={[styles.scroll, {flexGrow: 1}]}
+            ref={scrollViewRef}
+            onContentSizeChange={scrollToBottom}
+            showsVerticalScrollIndicator={false}
+          >
+            <MessageView messages={messages} />
+          </ScrollView>
+        </View>
+        <View>
+          <ClearPendingMatch pendingMatch={pendingMatch} />
+          <TextField pendingMatch={pendingMatch} />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -61,8 +81,8 @@ const ClearPendingMatch = ({
   };
   return (
     <View>
-      <Pressable onPress={_handleClearPendingMatch}>
-        <Text>Found your friend?</Text>
+      <Pressable style={styles.foundFriendButton} onPress={_handleClearPendingMatch}>
+        <Text style={styles.foundFriendText}>Found your friend?</Text>
       </Pressable>
     </View>
   );
@@ -85,15 +105,16 @@ const TextField = ({ pendingMatch }: { pendingMatch: PendingMatch | null }) => {
     }
   };
   return (
-    <View>
+    <View style={styles.messageSendBox}>
       <TextInput
         onChangeText={onChangeText}
         value={text}
         placeholder="Enter your message."
         keyboardType="default"
+        style={styles.textInput}
       />
-      <Pressable onPress={_handleMessageSend}>
-        <Text>Send</Text>
+      <Pressable onPress={_handleMessageSend} style={styles.sendButton}>
+        <Text style={styles.sendButtonText}>Send</Text>
       </Pressable>
     </View>
   );
@@ -112,22 +133,31 @@ const MessageView = ({ messages }: { messages: Message[] }) => {
     <View>
       {messages.map((message, i) => {
         if (isFromSender(message))
-          return <MessageBubble style={[styles.senderBubble, styles.messageText, styles.senderText]} key={i} message={message} />;
-        return <MessageBubble style={[styles.receiverBubble, styles.messageText, styles.receiverText]} key={i} message={message} />;
+          return <MessageBubble containerStyle={styles.senderBubble} textStyle={styles.senderText} key={i} message={message} />;
+        return <MessageBubble containerStyle={styles.receiverBubble} textStyle={styles.receiverText} key={i} message={message} />;
       })}
     </View>
   );
 };
 
-const MessageBubble = ({ style, message }: { style: object, message: Message }) => {
+const MessageBubble = ({ containerStyle, textStyle, message }: { containerStyle: object, textStyle: object, message: Message }) => {
   return (
-    <View style={styles.messageContainer}>
-      <Text style={style}>{message.content}</Text>
+    <View style={[styles.messageContainer, containerStyle]}>
+      <Text style={[styles.messageText, textStyle]}>{message.content}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  scroll: {
+    paddingHorizontal: 8,
+  },
   messageContainer: {
     marginBottom: 8,
   },
@@ -158,6 +188,39 @@ const styles = StyleSheet.create({
   },
   receiverText: {
     color: "black",
+  },
+  messageSendBox: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 8,
+  },
+  textInput: {
+    flex: 1,
+    marginRight: 8,
+  },
+  sendButton: {
+    marginLeft: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 4,
+    backgroundColor: '#0A84FF',
+  },
+  sendButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  foundFriendButton: {
+    height: 35,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  foundFriendText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
